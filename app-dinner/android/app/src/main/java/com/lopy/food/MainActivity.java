8e3596f7ae476c6d12007bdc10ddccaf018ae88a
Yao Liang <yaoliang0112@gmail.com>
2024-06-19 20:28:46 +0800

    LOPY-158: [android] Unable to add new card

--- app-dinner/android/app/src/main/java/com/lopy/food/MainActivity.java
+++ app-dinner/android/app/src/main/java/com/lopy/food/MainActivity.java
@@ -1,6 +1,6 @@
 package com.lopy.food;
 
-import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-public class MainActivity extends FlutterActivity {
+public class MainActivity extends FlutterFragmentActivity {
 }
